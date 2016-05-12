c2ff4bfd5c48a865e46ada18ddf8afab1e7b4891
GitiAzonx <silvio.gutierrez@he-arc.ch>
2016-05-12 21:02:59 +0200

    Correction classe person - ajout attribut movies

--- src/java/ch/hearc/ig/odi/movieapp/business/Person.java
+++ src/java/ch/hearc/ig/odi/movieapp/business/Person.java
@@ -5,20 +5,25 @@
  */
 package ch.hearc.ig.odi.movieapp.business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author silvio.gutierre
  */
 public class Person {
-    
+
     private Long id;
     private String firstName;
     private String lastName;
+    private List<Movie> movies;
 
     public Person(Long id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.movies = new ArrayList();
     }
 
     public Long getId() {
@@ -44,7 +49,13 @@ public class Person {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
-    
-    
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }   
+
 }
